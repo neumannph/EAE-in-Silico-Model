ba3c2af8f0f3f5865d608d077b3aa01e5a78e13b
Philippe Neumann <philippeneumannufjf@gmail.com>
2026-06-23 10:19:12 -0300

    Plot separado dos modelos de 21 e 58 dias

--- plotar.py
+++ plotar.py
@@ -2,8 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np 
 
-arquivo = 'dados1.csv'
-colunas = ['Tempo', 'Micróglias', 'Células Iba-1+', 'Oligodendrócitos', 'Citocinas Pró-Inflamatórias', 'Citocinas Anti-Inflamatórias']
+arquivo = 'dados21Dias.csv'
+colunas = ['Tempo', 'Micróglias Basal', 'Células Iba-1+', 'Oligodendrócitos', 'Citocinas Pró-Inflamatórias', 'Citocinas Anti-Inflamatórias']
 
 try:
     df = pd.read_csv(arquivo, sep=r',', header=0, names=colunas)
@@ -25,7 +25,7 @@ plt.rcParams.update({
 
 def printPicos():
     tempo = df['Tempo'].to_numpy()
-    microglias = df['Micróglias'].to_numpy()
+    microglias = df['Micróglias Basal'].to_numpy()
     celulasIba1 = df['Células Iba-1+'].to_numpy()
     oligodendrocitos = df['Oligodendrócitos'].to_numpy()
     citocinaPro = df['Citocinas Pró-Inflamatórias'].to_numpy()
@@ -64,9 +64,9 @@ def printPicos():
 # Função para plotar o modelo de 21 dias
 def plot21DaysModel(arquivo):
     try:
-        df = pd.read_csv(arquivo, sep=r',', header=0, names=colunas)
+        df_local = pd.read_csv(arquivo, sep=r',', header=0, names=colunas)
         
-        df.columns = colunas
+        df_local.columns = colunas
         
     except FileNotFoundError:
         print(f"Erro: Arquivo '{arquivo}' não encontrado.")
@@ -79,17 +79,17 @@ def plot21DaysModel(arquivo):
 
     plotMarkers21Days(ax)
 
-    # Plota as Células Iba-1+ (micróglias)
-    ax[0].plot(df['Tempo'], df['Micróglias'], label='Micróglias', linewidth=3, color = 'tab:purple')
+    # Plota as Células Iba-1+ (Micróglias Basal)
+    ax[0].plot(df_local['Tempo'], df_local['Micróglias Basal'], label='Micróglia Basal', linewidth=3, color = 'tab:purple')
 
-    # Plota as Células Iba-1+ (micróglias)
-    ax[0].plot(df['Tempo'], df['Células Iba-1+'], label='Células Iba-1+', linewidth=3, color = 'tab:red')
+    # Plota as Células Iba-1+ (Micróglias Basal)
+    ax[0].plot(df_local['Tempo'], df_local['Células Iba-1+'], label='Células Iba-1+', linewidth=3, color = 'tab:red')
 
     # Plota os Oligodendrócitos
-    ax[0].plot(df['Tempo'], df['Oligodendrócitos'], label='Oligodendrócitos', linewidth=3, color = 'tab:green')
+    ax[0].plot(df_local['Tempo'], df_local['Oligodendrócitos'], label='Oligodendrócitos', linewidth=3, color = 'tab:green')
 
     # Configurações Visuais
-    ax[0].set_title('Microglia e Oligodendrócito', fontsize = 11, fontweight='bold')
+    ax[0].set_title('Densidade de microglia basal, microglia ativada e oligodendrócito\n (sem tratamento)', fontsize = 12, fontweight='bold')
     ax[0].set_xlabel('Tempo (dias)', fontsize = 11, fontweight='bold')
     ax[0].set_ylabel('Densidade (cells/mm²)', fontsize = 11, fontweight='bold')
     ax[0].grid(True) 
@@ -97,31 +97,32 @@ def plot21DaysModel(arquivo):
     ax[0].tick_params(labelsize = 13)
 
     # Plota as Citocinas Pró-Inflamatórias
-    ax[1].plot(df['Tempo'], df['Citocinas Pró-Inflamatórias'], label='Citocinas Pró-Inflamatórias', linewidth=3, color = 'tab:orange')
+    ax[1].plot(df_local['Tempo'], df_local['Citocinas Pró-Inflamatórias'], label='Citocinas Pró-Inflamatórias', linewidth=3, color = 'tab:orange')
 
     # Plota as Citocinas Anti-Inflamatórias
-    ax[1].plot(df['Tempo'], df['Citocinas Anti-Inflamatórias'], label='Citocinas Anti-Inflamatórias', linewidth=3, color = 'tab:blue')
+    ax[1].plot(df_local['Tempo'], df_local['Citocinas Anti-Inflamatórias'], label='Citocinas Anti-Inflamatórias', linewidth=3, color = 'tab:blue')
 
     # Configurações Visuais
-    ax[1].set_title('Citocinas Pró e Anti-inflamatórias', fontsize = 11, fontweight='bold')
+    ax[1].set_title('Concentração de Citocinas Pró e Anti-inflamatórias', fontsize = 12, fontweight='bold')
     ax[1].set_xlabel('Tempo (dias)', fontsize = 11, fontweight='bold')
     ax[1].set_ylabel('Concentração (pg/ml)', fontsize = 11, fontweight='bold')
     ax[1].grid(True) 
     ax[1].legend(fontsize = 13) 
     ax[1].tick_params(labelsize = 13)
     plt.tight_layout() 
+    plt.ylim(0, 2000)
     plt.savefig('modelo_21_dias.jpg') 
 
 # Função para plotar os marcadores de 21 dias
 def plotMarkers21Days(ax):
-    ax[1].plot(21, 790, label='Pró', marker = '*', color = 'orange', markersize = 10)
-    ax[1].plot(21, 1730, label='Anti', marker = '*', color = 'blue', markersize = 10)
+    ax[1].plot(21, 790, marker = '*', color = 'orange', markersize = 10)
+    ax[1].plot(21, 1730, marker = '*', color = 'blue', markersize = 10)
 
 # Função para plotar o modelo de 58 dias
 def plot58DaysModel(arquivo):
     try:
-        df = pd.read_csv(arquivo, sep=r',', header=0, names=colunas)
-        df.columns = colunas
+        df_local = pd.read_csv(arquivo, sep=r',', header=0, names=colunas)
+        df_local.columns = colunas
         
     except FileNotFoundError:
         print(f"Erro: Arquivo '{arquivo}' não encontrado.")
@@ -132,17 +133,17 @@ def plot58DaysModel(arquivo):
 
     fig, ax = plt.subplots(1,2, figsize = (12, 5))
 
-    # Plota as Células Iba-1+ (micróglias)
-    ax[0].plot(df['Tempo'], df['Micróglias'], label='Micróglias', linewidth=3, color = 'tab:purple')
+    # Plota as Células Iba-1+ (Micróglias Basal)
+    ax[0].plot(df_local['Tempo'], df_local['Micróglias Basal'], label='Micróglia Basal', linewidth=3, color = 'tab:purple')
 
-    # Plota as Células Iba-1+ (micróglias)
-    ax[0].plot(df['Tempo'], df['Células Iba-1+'], label='Células Iba-1+', linewidth=3, color = 'tab:red')
+    # Plota as Células Iba-1+ (Micróglias Basal)
+    ax[0].plot(df_local['Tempo'], df_local['Células Iba-1+'], label='Células Iba-1+', linewidth=3, color = 'tab:red')
 
     # Plota os Oligodendrócitos
-    ax[0].plot(df['Tempo'], df['Oligodendrócitos'], label='Oligodendrócitos', linewidth=3, color = 'tab:green')
+    ax[0].plot(df_local['Tempo'], df_local['Oligodendrócitos'], label='Oligodendrócitos', linewidth=3, color = 'tab:green')
 
     # Configurações Visuais
-    ax[0].set_title('Microglia e Oligodendrócito', fontsize = 11, fontweight='bold')
+    ax[0].set_title('Densidade de microglia basal, microglia ativada e oligodendrócito\n(sem tratamento)', fontsize = 12, fontweight='bold')
     ax[0].set_xlabel('Tempo (dias)', fontsize = 11, fontweight='bold')
     ax[0].set_ylabel('Densidade (cells/mm²)', fontsize = 11, fontweight='bold')
     ax[0].grid(True) 
@@ -152,37 +153,48 @@ def plot58DaysModel(arquivo):
     plotMarkers58Days(ax)
 
     # Plota as Citocinas Pró-Inflamatórias
-    ax[1].plot(df['Tempo'], df['Citocinas Pró-Inflamatórias'], label='Citocinas Pró-Inflamatórias', linewidth=3, color = 'tab:orange')
+    ax[1].plot(df_local['Tempo'], df_local['Citocinas Pró-Inflamatórias'], label='Citocinas Pró-Inflamatórias', linewidth=3, color = 'tab:orange')
 
     # Plota as Citocinas Anti-Inflamatórias
-    ax[1].plot(df['Tempo'], df['Citocinas Anti-Inflamatórias'], label='Citocinas Anti-Inflamatórias', linewidth=3, color = 'tab:blue')
+    ax[1].plot(df_local['Tempo'], df_local['Citocinas Anti-Inflamatórias'], label='IL-10', linewidth=3, color = 'tab:blue')
 
     # Configurações Visuais
-    ax[1].set_title('Citocinas Pró e Anti-inflamatórias', fontsize = 11, fontweight='bold')
+    ax[1].set_title('Concentração de Citocinas Pró e Anti-inflamatórias\n(MOG 100μg)', fontsize = 12, fontweight='bold')
     ax[1].set_xlabel('Tempo (dias)', fontsize = 11, fontweight='bold')
     ax[1].set_ylabel('Concentração (pg/ml)', fontsize = 11, fontweight='bold')
     ax[1].grid(True) 
     ax[1].legend(fontsize = 13) 
     ax[1].tick_params(labelsize = 13)
     plt.tight_layout() 
+    plt.ylim(0, 2000)
     plt.savefig('modelo_58_dias.jpg')
 
-# Função para plotar os marcadores de 58 dias
+
+# Função para plotar os marcadores de 58 dias MOG 100ug
 def plotMarkers58Days(ax):
-    ax[1].plot(58, 400, label='Pró', marker = '*', color = 'orange', markersize = 10)
-    ax[1].plot(58, 2000, label='Anti', marker = '*', color = 'blue', markersize = 10)
+    ax[1].plot(7, 394, marker = '*', color = 'blue', markersize = 10)
+    ax[1].plot(10, 208, marker = '*', color = 'blue', markersize = 10)
+    ax[1].plot(14, 44, marker = '*', color = 'blue', markersize = 10)
+    ax[1].plot(21, 159, marker = '*', color = 'blue', markersize = 10)
+    ax[1].plot(58, 292, marker = '*', color = 'blue', markersize = 10)
+
+# Função para plotar os marcadores de 58 dias MOG 300ug
+""" def plotMarkers58Days(ax):
+    ax[1].plot(7, 249, marker = '*', color = 'blue', markersize = 10)
+    ax[1].plot(10, 126, marker = '*', color = 'blue', markersize = 10)
+    ax[1].plot(14, 375, marker = '*', color = 'blue', markersize = 10)
+    ax[1].plot(21, 161, marker = '*', color = 'blue', markersize = 10)
+    ax[1].plot(58, 192, marker = '*', color = 'blue', markersize = 10)
+ """
 
 print("============= PICOS 21 DIAS =============")
-printPicos()
-
-plot21DaysModel(arquivo)
+#printPicos()
+#plot21DaysModel(arquivo)
 
 print("============= PICOS 58 DIAS =============")
 printPicos()
 
-arquivo = 'dados2.csv'
+arquivo = 'dados58Dias.csv'
 plot58DaysModel(arquivo)
 
-plt.tight_layout() 
-plt.savefig('gráfico.jpg') 
 plt.show()
